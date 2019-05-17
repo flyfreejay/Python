f8aa6903dcf3e6674d398c2975439804dcf0f358
Jaehyung Lee <jaehyunglee@localhost.localdomain>
2019-05-16 21:05:46 -0400

    yes

---
+++ CCC/97 S/Sentences.py
@@ -0,0 +1,12 @@
+###INPUTS###
+n = int(input()) #n is the number of the datasets
+s = int(input()) #s is number of subjects
+v = int(input()) #v is the number of verbs
+o = int(input()) #o is the number of objects
+
+###VARIABLES###
+verbs = []
+objects = []
+subjects = []
+
+def MakeSentence()
