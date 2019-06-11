10ac6e5f706c2cc319952340e5e521442446308f
Jaehyung Lee <jaehyunglee-macbook@localhost.localdomain>
2019-06-10 20:10:05 -0400

    k

---
+++ CCC/02/0123456789.py
@@ -0,0 +1,124 @@
+'''
+    Date: 01/06/2019
+    Author: Jay Lee
+    Version: 1.0
+    Purpose: to find the prime numbers from 3 to 100
+'''
+
+i = str(input())
+
+def Write0():
+    print(" * * *")
+    print("*     *")
+    print("*     *")
+    print("*     *")
+    print("")
+    print("*     *")
+    print("*     *")
+    print("*     *")
+    print(" * * *")
+def Write1():
+    print("      *")
+    print("      *")
+    print("      *")
+    print("")
+    print("      *")
+    print("      *")
+    print("      *")
+def Write2():
+    print(" * * * ")
+    print("      *")
+    print("      *")
+    print("      *")
+    print(" * * * ")
+    print("*      ")
+    print("*      ")
+    print("*      ")
+    print(" * * * ")
+def Write3():
+    print(" * * * ")
+    print("      *")
+    print("      *")
+    print("      *")
+    print(" * * * ")
+    print("      *")
+    print("      *")
+    print("      *")
+    print(" * * * ")
+def Write4():
+    print("*     *")
+    print("*     *")
+    print("*     *")
+    print(" * * * ")
+    print("      *")
+    print("      *")
+    print("      *")
+def Write5():
+    print(" * * * ")
+    print("*")
+    print("*")
+    print("*")
+    print(" * * * ")
+    print("      *")
+    print("      *")
+    print(" * * *")
+def Write6():
+    print(" * * *")
+    print("*      ")
+    print("*      ")
+    print("*      ")
+    print(" * * *")
+    print("*     *")
+    print("*     *")
+    print("*     *")
+    print(" * * *")
+def Write7():
+    print(" * * *")
+    print("      *")
+    print("      *")
+    print("      *")
+    print("       ")
+    print("      *")
+    print("      *")
+    print("      *")
+def Write8():
+    print(" * * *")
+    print("*     *")
+    print("*     *")
+    print("*     *")
+    print(" * * *")
+    print("*     *")
+    print("*     *")
+    print("*     *")
+    print(" * * *")
+def Write9():
+    print(" * * *")
+    print("*     *")
+    print("*     *")
+    print("*     *")
+    print(" * * *")
+    print("      *")
+    print("      *")
+    print("      *")
+    print(" * * *")
+
+if i =="1":
+    Write1()
+elif i =="2":
+    Write2()
+elif i== "3":
+    Write3()
+elif i == "4":
+    Write4()
+elif i == "5":
+    Write5()
+elif i =="6":
+    Write6()
+elif i == "7":
+    Write7()
+elif i == "8":
+    Write8()
+elif i == "9":
+    Write9()
+else:
+    Write0()
