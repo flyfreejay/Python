91be5f40b81d54305301d0f0b59020a5316e4dac
jaylee3 <42979197+jaylee3@users.noreply.github.com>
2019-08-30 19:43:37 -0400

    ang

--- Django/wisdompets/adoptions/views.py
+++ Django/wisdompets/adoptions/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from django.core.exceptions import *
 
-# Create your views here.
+from .models import Pet
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request, 'home.html',{'pets':pets})
+
+def pet_detail(request, id):
+    try: 
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet Not Found')
+    return render(request, 'pet_detail.html',{'pet':pet})
+
+def getting_input(request):
+    # pets = Pet.objects.all()
+    # print(request.GET)
+    # print(request.POST)
+    if request.method == "POST":
+        title = request.POST.get('title')
+        print(title)
+        context = {}
+        return render(request,  'getting_input.html',context)
